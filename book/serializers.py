33f20b286ae1cb882e381a86148011eb07862b85
duraprakash <duraprakash141@gmail.com>
2024-05-08 18:21:29 +0545

    removed unwanted serializers

--- book/serializers.py
+++ book/serializers.py
@@ -21,34 +21,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-        
-# create override serializers
-class CategoryCreateSerializer(serializers.ModelSerializer):
-    category = serializers.StringRelatedField()
-    athor = serializers.StringRelatedField(many=True)
-    class Meta:
-        model = Category
-        fields = '__all__'
-        
-    # def create(self, validated_data):
-    #     name= validated_data.get('name',None)
-    #     user=Category.objects.create(
-    #         name=name,
-    #     )
-    #     return user
-
-
-class SubCategoryCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubCategory
-        fields = '__all__'
-
-class AuthorCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
-
-class BookCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
