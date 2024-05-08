713493415d9f675f62e26cf859d85a897381164a
duraprakash <duraprakash141@gmail.com>
2024-05-08 08:59:37 +0545

    JWT token, refresh, verify, blacklist in user urls.py done

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from .views import UserCreateView, Seller, UserListView, UserRetrieveView, UserUpdateView, UserDeleteView, LoginView, LogoutView
+from rest_framework_simplejwt.views import (
+    TokenObtainSlidingView,
+    TokenRefreshSlidingView,
+    
+    TokenVerifyView,
+    
+    TokenBlacklistView,
+)
 
 urlpatterns = [
     path('', UserListView.as_view(), name='users'),
@@ -10,4 +18,11 @@ urlpatterns = [
     
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    
+    path('api/token/', TokenObtainSlidingView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+    
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
